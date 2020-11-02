d38f0666900c978ccf4bed0f12dbf2020a0e471e
yslsaenderin <yerinnj@gmail.com>
2020-11-03 00:18:24 +0900

    Hello 클래스에 message 멤버 변수 추가

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,9 +1,12 @@
 package git_test2;
 
 public class Hello {
+    
+    String message = "안녕하세요";
 
     public void say() {
-        System.out.println("안녕하세");
+        System.out.println(message);
     }
 
 }
+
